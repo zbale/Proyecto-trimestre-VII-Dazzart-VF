2df57477f30fdada4c8753c3483450bceea78f13
jose zabaleta <jopsezabaleta5@gmail.com>
2025-11-07 17:53:16 -0500

    Change server listening address to 0.0.0.0

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ const app = createApp();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor backend corriendo en http://0.0.0.0:${PORT}`);
 });
