74a3799600a6feb2963e2000f1124c8543fabd62
Jose_Zabaleta <jopsezabaleta5@gmail.com>
2025-11-11 22:16:40 -0500

    movil: add Android network security config for HTTP cleartext traffic

---
+++ movil/android/README.md
@@ -0,0 +1,8 @@
+// Esta carpeta contiene configuraciones de Android que Expo/EAS Build utilizará
+
+// El archivo network_security_config.xml permite tráfico HTTP a estos dominios:
+// - 67.202.48.5 (backend)
+// - localhost
+// - 127.0.0.1
+
+// El archivo AndroidManifest.xml declara usesCleartextTraffic para compatibilidad
---
+++ movil/expo.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  expo: {
+    name: 'Dazzart Components',
+    slug: 'movil',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './src/assets/logo.png',
+    scheme: 'dazzart-components',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.zbale.movil',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './src/assets/logo.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      package: 'com.zbale.movil',
+      // Network security config se maneja via android/ folder
+      // Ver: android/app/src/main/res/xml/network_security_config.xml
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './src/assets/logo.png',
+    },
+    splash: {
+      image: './src/assets/logo.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './src/assets/logo.png',
+          imageWidth: 300,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: '60a085e2-f299-4b11-b258-6dae20f85fab',
+      },
+      EXPO_PUBLIC_API_URL: 'http://67.202.48.5:3001',
+    },
+  },
+};
